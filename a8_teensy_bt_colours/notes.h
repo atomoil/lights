450dbc6d95f052ae8ee6f60db163e62ba454e11a
Adam Hoyle <adam@adamhoyle.co.uk>
2018-06-04 22:06:35 +0100

    create version for storing / changing palette

---
+++ a8_teensy_bt_colours/notes.h
@@ -0,0 +1,14 @@
+/*
+ * 4.7.15 add basic light sequence stuff to test
+ * 25.6.17 add RTC read functions
+ * Use Char instead of int for control charecter- DONE
+ * intitial state of filter is high - use inverted logic to avoid lamp on at boot.DONE
+ * 15.6.2017 initial app. with lLP filter and noise filter.
+ */
+// remove setled function
+//check why ds1307 is stopped = battery?? on crank and small brown
+
+ // grey crank: 1800,1400 55 led
+ // tall grey: 1500, 800 55 led
+ // small brown:800,500 50 led
+ 
