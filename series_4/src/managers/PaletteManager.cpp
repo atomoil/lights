7e483a4473539cf7d6a74a0a3828cf63eac87281
Adam Hoyle <atomoil@gmail.com>
2020-12-07 23:40:40 +0000

    add initial palette manager

---
+++ series_4/src/managers/PaletteManager.cpp
@@ -0,0 +1,56 @@
+#include "PaletteManager.h"
+
+void PaletteManager::setup()
+{
+    totalPalettes = sizeof(palette) / sizeof(palette[0]);
+    loadPalette();
+}
+void PaletteManager::loadPalette()
+{
+    int savedPalette[5][2] = {};
+  EEPROM.get(0,savedPalette);
+  int paletteSize = sizeof(savedPalette);
+  
+  if (paletteSize > 0){ // only update if we have values
+    for(int i=0;i<totalPalettes;i++){
+      palette[i][0] = savedPalette[i][0];
+      palette[i][1] = savedPalette[i][1];
+    }
+  }
+}
+
+// pl:10:11:20:21:30:31:40:41:50:51:60:61
+// pl:10:100:20:100:30:100:40:100:50:100
+// 
+void PaletteManager::setPaletteFromPlCode(char input[100])
+{
+    char *text = strtok(input,":"); // remove first 'pl'
+    int col = 0;
+    int i = 0;
+    int row = 0;
+    int elem = 0;
+    while (text != 0 && i < totalPalettes*2){
+      row = i / 2;
+      elem = i % 2;
+      text = strtok(0,":");
+      col = atoi( text );
+      if (elem == 0){
+        palette[row][0] = convertHue(col);
+      } else {
+        palette[row][1] = convertSat(col);
+      }
+      Serial.print(row);
+      Serial.print("/");
+      Serial.print(elem);
+      Serial.print(": ");
+      Serial.println(col);
+      i++;
+    }
+}
+
+int PaletteManager::convertHue(float f){
+  return (f/360.0)*255.0;
+}
+int PaletteManager::convertSat(float f){
+  return (f/100.0)*255.0;
+}
---
+++ series_4/src/managers/PaletteManager.h
@@ -0,0 +1,21 @@
+#ifndef PALETTE_MANAGER_HPP_INCLUDED
+#define PALETTE_MANAGER_HPP_INCLUDED
+
+#include <Arduino.h>
+#include <EEPROM.h>
+
+class PaletteManager
+{
+private:
+    int palette[5][3] = { {40,255}, {80,255}, {120,255}, {140,255}, {180,255} };
+    int totalPalettes;
+    void loadPalette();
+    void savePalette();
+    int convertHue(float f);
+    int convertSat(float f);
+public:
+    void setup();
+    void setPaletteFromPlCode(char input[100]);
+};
+
+#endif
