904875bab99da3b5a5eeaabfdd09e28522e74560
ian <ian@ians-MBP.home.net>
2020-12-08 15:51:47 +0000

    Tidy and update test software

--- examples/TeensyAPA102-2/notes.h
+++
@@ -1,9 +0,0 @@
-/*   // --- FOR TEENSYTOUCH PCB ONLY ---//
- *    
- *    Sensitivity for each light
- *    analogWrite(DAC_PIN, 4095); //sensitivity for each light 4095 black one, 200 short brown
- *  LC board runs faster so slow down loop
- * check switch logic and delays 
- * change hold time??
- * 
- */
