d36f5c453bbe2813086018b950681d8c8632c514
Adam Hoyle <atomoil@gmail.com>
2020-12-14 16:42:32 +0000

    improve definition of lamps

--- series_4/src/lamps.h
+++ series_4/src/lamps.h
@@ -1,20 +1,22 @@
 
-//const int touchTriggerOn = 700; // 700 // 1800
-//const int touchTriggerOff = 500; // 500
-#define TOUCH_ON 700
-#define TOUCH_OFF 500
+
+#define TOUCH_ON 2400
+#define TOUCH_OFF 1000
 // #define SUPPORTS_FFT 1
 
-//char hardware_version[] = "4.2";
+
 #define LAMP_HARDWARE_VERSION "4.2"
 
 #ifdef SUPPORTS_FFT
-//char supports[] = "<fft=1,2/>";
 #define LAMP_SUPPORTS "<fft=1,2/>"
 #endif
 
+
+#ifndef LAMP_SUPPORTS
 #ifndef SUPPORTS_FFT
-//char supports[] = "";
 #define LAMP_SUPPORTS ""
-#endif
+#else
+#define LAMP_SUPPORTS "<fft=1,2/>"
+#endif // ifndef SUPPORTS_FFT
+#endif // ifndef LAMP_SUPPORTS
 
