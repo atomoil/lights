6e1473bd9f1fba4e89089089e87530020869df6a
Adam Hoyle <atomoil@gmail.com>
2020-12-19 17:39:12 +0000

    stop inadvertently setting LEDs to full on even when mode hasn’t been activated

--- series_4/src/modes/ColourCyclingRGBMode.cpp
+++ series_4/src/modes/ColourCyclingRGBMode.cpp
@@ -27,17 +27,7 @@ ColourCyclingRGBMode::ColourCyclingRGBMode(
 }
 
 void ColourCyclingRGBMode::setup() {
-    setAllLEDsToRGB(255,255,255,0); // this should never actually be seen!
     restart();
-    /*
-    // debug for passing in array
-    for(int i=0;i<countColours;i++) {
-        char message[40];
-        int off = i*3;
-        sprintf(message,"%i > %i,%i,%i",i,colours[off],colours[off+1],colours[off+2]);
-        Serial.print("ColourCycling ");
-        Serial.println(message);
-    }*/
 }
 
 void ColourCyclingRGBMode::restart()
