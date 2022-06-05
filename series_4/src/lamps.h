c7bd1bfb0fbd37e8f90606ad10e05b28c99e5148
Adam Hoyle <atomoil@gmail.com>
2022-06-05 01:25:17 +0100

    Add settings for new lamps

--- series_4/src/lamps.h
+++ series_4/src/lamps.h
@@ -1,9 +1,11 @@
 
 #define LAMP_S7_01_to_05 1
 #define LAMP_S7_05_and_06_STARBURST 2
-#define LAMP_S7_07_and_08_and_09_and_10 3
+#define LAMP_S7_07_to_10_STARBURST 3
+#define LAMP_S8_01_to_02_STARBURST 4
+#define LAMP_S8_03_to_05_ARTDECO 5
 
-#define LAMP_CURRENT LAMP_S7_07_and_08_and_09_and_10
+#define LAMP_CURRENT LAMP_S8_03_to_05_ARTDECO
 
 
 
@@ -16,19 +18,37 @@
 
 #endif
 
-  #if LAMP_CURRENT == LAMP_S7_05_and_06_STARBURST
+#if LAMP_CURRENT == LAMP_S7_05_and_06_STARBURST
 
-  #define NUM_LEDS 14
-  #define TOUCH_ON 900
-  #define TOUCH_OFF 500
-  #define SUPPORTS_FFT 1
+#define NUM_LEDS 14
+#define TOUCH_ON 900
+#define TOUCH_OFF 500
+#define SUPPORTS_FFT 1
 
-  #endif
+#endif
 
-#if LAMP_CURRENT == LAMP_S7_07_and_08_and_09_and_10
+#if LAMP_CURRENT == LAMP_S7_07_to_10_STARBURST
 
 #define NUM_LEDS 15
 #define TOUCH_ON 1200
+#define TOUCH_OFF 1000
+#define SUPPORTS_FFT 1
+
+#endif
+
+#if LAMP_CURRENT == LAMP_S8_01_to_02_STARBURST
+
+#define NUM_LEDS 15
+#define TOUCH_ON 1200
+#define TOUCH_OFF 1000
+#define SUPPORTS_FFT 1
+
+#endif
+
+#if LAMP_CURRENT == LAMP_S8_03_to_05_ARTDECO
+
+#define NUM_LEDS 10
+#define TOUCH_ON 1500
 #define TOUCH_OFF 1000
 #define SUPPORTS_FFT 1
 
